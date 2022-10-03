b1a77f772571fbd6fa37264e1eaafeb103c44841
abhinav-exp <kabhinav482@gmail.com>
2022-10-03 13:41:02 +0530

    FPI

--- react_project/src/components/Menu.js
+++ react_project/src/components/Menu.js
@@ -1,13 +1,28 @@
 import { Button } from "react-bootstrap";
 import { useState } from "react";
 
+function Profile(){
+    return (<div> <img src="https://localhost:8000/" alt="Image Not Available" width="2000" height="2000" ></img></div>)
+}
+
+function InfoAsset(){
+    return (<div>Info Asset</div>)
+}
+
+function InfoProfile(){
+    return (<div>Info Profile</div>)
+}
+
 export default function Menu(){
 
     const [num, handleNum] = useState(0);
 
+    const V = [<Profile></Profile>, <InfoAsset></InfoAsset>, <InfoProfile></InfoProfile>]
+
     return (<div className="Menu">
-        <Button variant={(num === 0)? "primary" : "light"} className="perc30" onClick={() => handleNum(0)}> Sell </Button>
-        <Button variant={(num === 1)? "primary" : "light"} className="perc30" onClick={() => handleNum(1)}> Assets </Button>
-        <Button variant={(num === 2)? "primary" : "light"} className="perc30" onClick={() => handleNum(2)}> Explore </Button>
+        <Button variant={(num === 0)? "primary" : "light"} className="perc30" onClick={() => handleNum(0)}> Profile </Button>
+        <Button variant={(num === 1)? "primary" : "light"} className="perc30" onClick={() => handleNum(1)}> Info Asset </Button>
+        <Button variant={(num === 2)? "primary" : "light"} className="perc30" onClick={() => handleNum(2)}> Info Profile </Button>
+        {V[num]}
     </div>);
 }
