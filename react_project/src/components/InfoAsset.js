b1b4c851827f2a9517821a3343c818b8672c76c8
abhinav-exp <kabhinav482@gmail.com>
2022-10-19 00:57:12 +0530

    add pictures

--- react_project/src/components/InfoAsset.js
+++ react_project/src/components/InfoAsset.js
@@ -17,17 +17,61 @@ const OwnerCard = (props) => {
             handlePicstr(r[2]);
         })
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+    })
 
     const Backend_url = process.env.REACT_APP_BACKEND_URL;
 
-    return (<div>
-        <img src={Picstr === "" ? NoPic : Backend_url + "/show/" + Picstr} alt="Profile Pic" />
-        <div>
+    return (<div className='OwnerCard'>
+        <img className='LN' src={Picstr === "" ? NoPic : Backend_url + "/show/" + Picstr} alt="Profile Pic" />
+        <div className='OwnerDetails'>
             <Button variant="light" disabled={true}> {First_Name === "" ? "[Not Avaliable]" : First_Name} </Button>
             <Button variant="light" disabled={true}> {Last_Name === "" ? "[Not Avaliable]" : Last_Name} </Button>
+            <br></br>
             <Button> {props.OwnAddr} </Button>
         </div>
+    </div>)
+}
+
+const TransferProduct = (props) => {
+    return (<div className='Feat' >
+        <input type="text" placeholder='Address To Transfer Ownship To'></input>
+        <Button variant='warning' >Submit</Button>
+    </div>)
+}
+
+const AddPictures = (props) => {
+
+    const [inputFile, handleinputFile] = useState();
+
+    const Backend_url = process.env.REACT_APP_BACKEND_URL;
+
+    const fun = () => {
+        var formData = new FormData();
+        formData.append("FK", inputFile);
+        fetch(
+            Backend_url + "/add",
+            {
+                method: 'POST',
+                body: formData,
+            }
+        ).then(r => {
+            if(r.ok){
+                r.text().then(a => {
+                    props.contAbi.methods.Add_Picture(a, props.enquiry).estimateGas()
+                    .then(gas => {
+                        props.contAbi.methods.Add_Picture(a, props.enquiry).send({
+                            from : props.myacc,
+                            gas : gas + 1000000
+                        })
+                    })
+                })
+            }
+        })
+    }
+
+    return (<div className='Feat' >
+        <input type="file" onChange={e => handleinputFile(e.target.files[0])} ></input>
+        <Button variant='warning' onClick = {fun} >Submit</Button>
     </div>)
 }
 
@@ -41,6 +85,8 @@ export default function InfoAsset(props){
     const [ProductDesp, handleProductDesp] = useState("");
     const [ProductImg,  handleProductImg] = useState([]);
     const [ProductAuth, handleProductAuth] = useState([]);
+    const [FeatureNum, handleFeatureNum] = useState(0);
+    const [OwnershipBool, handleOwnershipBool] = useState(false);
 
     const fun = (e) => {
         props.handleEnquiry(tempEnq);
@@ -70,6 +116,8 @@ export default function InfoAsset(props){
     }
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
+
+
     useEffect(() => {
         console.log("useEffect called");
         if(props.enquiry < 0){
@@ -80,6 +128,8 @@ export default function InfoAsset(props){
         else{
             console.log("else called")
             handleErrorBool(false);
+            handleOwnershipBool(false);
+            console.log("setting own false")
             console.log(props.enquiry);
             props.contAbi.methods.Get_Asset_of_No(props.enquiry).call()
             .then(r => {
@@ -88,6 +138,23 @@ export default function InfoAsset(props){
                 // console.log(r[1])
                 // handleProductImg(r[2]);
                 // handleLoadBool(false);
+
+                props.contAbi.methods.Get_My_Assets_Nos().call({
+                    from : props.myacc
+                })
+                .then(r => {
+                    console.log(r)
+                    for(let i=0; i<r.length; i++)
+                    {
+                        if(parseInt(r[i]) === parseInt(props.enquiry)){
+                            handleOwnershipBool(true);
+                            console.log("setting own true")
+                            console.log(props.enquiry)
+                            break;
+                        }
+                    }
+                })
+
                 props.contAbi.methods.Get_Pictures(props.enquiry).call()
                 .then(res => {
                     handleProductImg(PicFun(res));
@@ -107,28 +174,53 @@ export default function InfoAsset(props){
 
     const Backend_url = process.env.REACT_APP_BACKEND_URL;
 
+    const Features = [
+        <div></div>, 
+        <TransferProduct></TransferProduct>, 
+        <AddPictures myacc = {props.myacc} contAbi = {props.contAbi} enquiry = {props.enquiry} ></AddPictures>
+    ]
+
+    const funTransferProduct = () => {
+        if(FeatureNum === 1)
+            handleFeatureNum(0);
+        else
+            handleFeatureNum(1);
+    }
+
+    const funAddPictures = () => {
+        if(FeatureNum === 2)
+            handleFeatureNum(0);
+        else
+            handleFeatureNum(2);
+    }
+
     return (<div>
         <input type="number"  className="InfoNum" value={tempEnq} onInput={e => handleTempEnq(e.target.value)} ></input>
         <input type="submit" onClick={fun} ></input>
         <br></br>
-        <Button>Product Key : {props.enquiry}</Button>
-        <Button>Add Pictures</Button>
-        <Button>Transfer Product</Button>
+        <Button className='PK' >Product Key : {props.enquiry}</Button>
         {(ErrorBool)?<Alert variant='danger' >This number is invalid</Alert>:
-         <div className='IAIP'>
-            <div className='IAI0'>
-                <Alert className='IAI01' variant="danger" > Product Name : <br></br> {ProductName}</Alert>
-                <Alert className='IAI02' variant='info'> Product Description : <br></br> {ProductDesp} </Alert>
+        <div className = 'mega'>
+            <div hidden = {(OwnershipBool === false)}>
+                <Button variant='info' className='IAB' onClick = {funAddPictures} >Add Pictures</Button>
+                <Button variant='info' className='IAB' onClick = {funTransferProduct} >Transfer Product</Button>
+                {Features[FeatureNum]}
             </div>
-            <div className='IAI'>
-                {ProductImg.map(e => {
-                    return (<img className='IAIC' src={(e !== "")?Backend_url + "/show/" + e:NoPic} alt="Product Pic" ></img> )
-                })}
-            </div>
-            <div className='IAIU'>
-                {ProductAuth.map(e => {
-                    return (<OwnerCard OwnAddr = {e} contAbi = {props.contAbi} ></OwnerCard>)
-                })}
+            <div className='IAIP'>
+                <div className='IAI0'>
+                    <Alert className='IAI01' variant="danger" > Product Name : <br></br> {ProductName}</Alert>
+                    <Alert className='IAI02' variant='info'> Product Description : <br></br> {ProductDesp} </Alert>
+                </div>
+                <div className='IAI'>
+                    {ProductImg.map(e => {
+                        return (<img className='IAIC' src={(e !== "")?Backend_url + "/show/" + e:NoPic} alt="Product Pic" ></img> )
+                    })}
+                </div>
+                <div className='IAIU'>
+                    {ProductAuth.map(e => {
+                        return (<OwnerCard OwnAddr = {e} contAbi = {props.contAbi} ></OwnerCard>)
+                    })}
+                </div>
             </div>
         </div>}
     </div>)
